bdf72342536a5b4e83f9238693c1a9be155ab477
MatthewSmickle <matt.smickle.o@gmail.com>
2018-01-31 12:39:24 -0500

    Capche v2

--- Acsl/Acsl/Program.cs
+++ Acsl/Acsl/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Acsl
 {
@@ -10,6 +11,9 @@ namespace Acsl
     {
         static void Main(string[] args)
         {
+            String input = File.ReadAllText(@"C:\Users\eahscs\Desktop\text.txt.txt");
+            String[] lines = input.Split('\n');
+
         }
     }
 }
