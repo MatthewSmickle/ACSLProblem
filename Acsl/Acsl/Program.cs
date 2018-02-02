31d2f8d506abcb91117eb0f17a8cbc7001a65bd4
MatthewSmickle <matt.smickle.o@gmail.com>
2018-02-02 13:23:06 -0500

    meh

--- Acsl/Acsl/Program.cs
+++ Acsl/Acsl/Program.cs
@@ -13,11 +13,12 @@ namespace Acsl
         {
             String input = File.ReadAllText(@"C:\Users\eahscs\Desktop\text.txt.txt");
             String[] lines = input.Split('\n');
-            int[] pos;
+            List<int> pos = new List<int> { };
             bool lp = false, rp = false, lb = false, rb = false, lc = false, rc = false;
             for(int i = 0; i < lines.Length; i++)
             {
                 lines[i] = lines[i].Replace(" ", "");
+                String line = lines[i];
                 if (lines[i].Contains("("))
                 {
                     lp = true;
@@ -44,14 +45,34 @@ namespace Acsl
                 }
 
 
+                if(lb = true && rb == false)
+                {
+                    
+                    for(int j = 0; j < line.Length; j++)
+                    {
+                        if(line.IndexOf(")") < j)
+                        {
+                            if (("+-/*}".Contains(line.Substring(j, 1))))
+                            {
+                                pos.Add(j + 1);
+                            }
+
+                        }
+                    }
+                }
+
+                if(lp == false && rp == true)
+                {
+
+                }
             
 
-                for(int j = 0; j < pos.Length - 1; j++)
+                for(int j = 0; j < pos.Count - 1; j++)
                 {
-                    Console.Write(pos[i] + ", ");
+                    Console.Write(pos[j] + ", ");
                 
                 }
-                Console.WriteLine(pos[pos.Length]);
+                Console.WriteLine(pos[pos.Count-1]);
                
             }
             
