d6ca3bd6752520428dd460562c6e679e20e621fd
MatthewSmickle <31512911+MatthewSmickle@users.noreply.github.com>
2018-02-01 22:56:33 -0500

    some work

--- Acsl/Acsl/Program.cs
+++ Acsl/Acsl/Program.cs
@@ -13,6 +13,48 @@ namespace Acsl
         {
             String input = File.ReadAllText(@"C:\Users\eahscs\Desktop\text.txt.txt");
             String[] lines = input.Split('\n');
+            int[] pos;
+            bool lp = false, rp = false, lb = false, rb = false, lc = false, rc = false;
+            for(int i = 0; i < lines.Length; i++)
+            {
+                lines[i] = lines[i].Replace(" ", "");
+                if (lines[i].Contains("("))
+                {
+                    lp = true;
+                }
+                if (lines[i].Contains(")"))
+                {
+                    rp = true;
+                }
+                if (lines[i].Contains("["))
+                {
+                    lb = true;
+                }
+                if (lines[i].Contains("]"))
+                {
+                    rb = true;
+                }
+                if (lines[i].Contains("{"))
+                {
+                    lc = true;
+                }
+                if (lines[i].Contains("}"))
+                {
+                    rc = true;
+                }
+
+
+            
+
+                for(int j = 0; j < pos.Length - 1; j++)
+                {
+                    Console.Write(pos[i] + ", ");
+                
+                }
+                Console.WriteLine(pos[pos.Length]);
+               
+            }
+            
 
         }
     }
